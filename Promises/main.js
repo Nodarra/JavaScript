df6b974413e9bd05f32b1c6a7e6a6dd633c5bdd0
Nodarra <sisicdino4@gmail.com>
2021-04-08 12:08:19 +0200

    Testing async await functions

--- Promises/main.js
+++ Promises/main.js
@@ -1,42 +1,46 @@
-function fetchAndDecode(url, type){
-  return fetch(url).then(response => {
-    if(!response.ok){
-      throw new Error(`Error status: ${response.status}`);
-    }else{
-      if(type === 'blob'){
-        return response.blob();
-      }else if(type === 'text'){
-        return response.text();
-      }
-    }
-  }).catch(e => {
-    console.log(`There has been a problem fatching resource ${url}`)
-  }).finally(() => {
-    console.log("Fetch attempt finished")
-  })
+async function fetchAndDecode(url, type){
+  let response = await fetch(url);
+
+  let content;
+
+  if(!response.ok){
+    throw new Error('Erro fetching the data')
+  }
+
+  if(type === 'blob'){
+    content = await response.blob();
+  }else if(type === 'text'){
+    content = await response.text();
+  }
+
+  return content;
 }
 
-let coffee = fetchAndDecode('./images/coffee.jpg', 'blob');
-let tea = fetchAndDecode('./images/tea.jpg', 'blob');
-let description = fetchAndDecode('description.txt', 'text');
+async function displayContent(){
+  let coffee = fetchAndDecode('./images/coffee.jpg', 'blob');
+  let tea = fetchAndDecode('./images/tea.jpg', 'blob');
+  let descriptionText = fetchAndDecode('description.txt', 'text');
 
-Promise.all([coffee, tea, description]).then(values => {
-  console.log(values);
+  let values = await Promise.all([coffee, tea, descriptionText]);
 
   let objectURL1 = URL.createObjectURL(values[0]);
-  let objectURL2 = URL.createObjectURL(values[1]);
-  let descriptionText = values[2];
+  let objcetURL2 = URL.createObjectURL(values[1]);
+  let description = values[2];
 
   let img1 = document.createElement('img');
   let img2 = document.createElement('img');
-
+  
   img1.src = objectURL1;
-  img2.src = objectURL2;
+  img2.src = objcetURL2;
 
   document.body.appendChild(img1);
   document.body.appendChild(img2);
 
   let text = document.createElement('p');
-  text.textContent = descriptionText;
+
+  text.textContent = description;
+
   document.body.appendChild(text);
-});
+}
+
+displayContent().catch((e) => console.log(e));
